2e3b360926665cd0b879ec05443ed2535785ca6e
Abhishek Yadav <abhishekyadav2k16@gmail.com>
2024-08-21 22:41:18 +0530

    understood react flow

---
+++ 01viteReact/src/Hatcoder.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hatcoder = () => {
+  return <div>Hatcoder is Coming</div>;
+};
+
+export default Hatcoder;
--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Hatcoder from './Hatcoder.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Hatcoder/>
   </StrictMode>,
 )
