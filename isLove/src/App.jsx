a4d07be7608d7fbd48d78f8b39f50e6f6fb923e3
Abhishek Yadav <abhishekyadav2k16@gmail.com>
2024-08-24 22:50:21 +0530

    resolved issue

---
+++ isLove/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1> Check She loves you or not </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
